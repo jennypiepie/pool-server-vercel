23ae9bbbb53344f64f833f7df4b4eba8c004f953
派派 <2416655925@qq.com>
2023-08-09 22:57:17 +0800

    add lib

--- api/getUsername.ts
+++ api/getUsername.ts
@@ -1,27 +1,13 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { MongoClient, ServerApiVersion } from 'mongodb'
-
-const uri = "mongodb+srv://jennypie:zjn20010314@cluster0.vqqlnds.mongodb.net/?retryWrites=true&w=majority";
+import { Db } from 'mongodb'
+import { connectToDb } from '../lib/db';
 
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-  });
-
-  try {
-    await client.connect();
-    const db = await client.db('pool');
-    var result = await db.collection('user').find({}).toArray();
-    // console.log(client)
-    // console.log(db)
-    // console.log(result)
-    res.status(200).json(result);
-  } finally {
-    await client.close();
+  const db: Db | null = await connectToDb() || null;
+  if (db) {
+    const collection = db.collection('user');
+    const users = await collection.find({}).toArray();
+    res.status(200).json(users);
   }
 }
