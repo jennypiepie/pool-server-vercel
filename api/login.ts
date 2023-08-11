0834bb3e15f4493d1378e90c02af7ded0132d2ae
派派 <2416655925@qq.com>
2023-08-11 16:38:25 +0800

    status code

--- api/login.ts
+++ api/login.ts
@@ -6,25 +6,24 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
     if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
     const { username, password } = req.body;
     if (!username || !password) {
-        res.status(414).send('参数错误');
+        res.status(414).json({message:'参数错误'});
     }
 
     const db: Db | null = await connectToDb() || null;
     if (db) {
-        // const username = 'pool';
-        // const password = '12345';
         const collection = db.collection('user');
         const users = await collection.find({username}).toArray();
         if (!users.length) {
-            res.status(404).send('该用户不存在');
+            res.status(404).json({message:'该用户不存在'});
         } else {
             if (password !== users[0].password) {
-                res.status(414).send('密码错误');
+                res.status(414).json({message:'密码错误'});
             } else {
-                res.status(200).json(users[0]);
+                const result = Object.assign(users[0],{message:"登录成功"})
+                res.status(200).json(result);
             }
         }
     } else {
-        res.status(502).send('数据库连接失败');
+        res.status(502).json({message:'数据库连接失败'});
     }
 }
