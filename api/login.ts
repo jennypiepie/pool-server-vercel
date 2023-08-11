d7b67b27e577a67b614814d4a2de44c8970a5087
派派 <2416655925@qq.com>
2023-08-11 14:50:52 +0800

    cors

--- api/login.ts
+++ api/login.ts
@@ -3,6 +3,7 @@ import { Db } from 'mongodb'
 import { connectToDb } from '../lib/db';
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
+    if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
     const db: Db | null = await connectToDb() || null;
     if (db) {
         const { username, password } = req.body;
