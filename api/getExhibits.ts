3e9af354abfb2c56fd224f9ba58261b7d5af7e4e
派派 <2416655925@qq.com>
2023-08-13 00:10:32 +0800

    likeslist

--- api/getExhibits.ts
+++ api/getExhibits.ts
@@ -4,11 +4,15 @@ import { connectToDb } from '../lib/db';
 
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  if(req.method === 'OPTIONS') { return res.json(({ body: "OK" })) }
+  if (req.method === 'OPTIONS') { return res.json(({ body: "OK" })) }
+  const { username } = req.body;
+  
   const db: Db | null = await connectToDb() || null;
   if (db) {
     const collection = db.collection('exhibits');
-    const exhibits = await collection.find({}).toArray();
+    const exhibits = username ?
+      await collection.find({ likes: username }).toArray() :
+      await collection.find({}).toArray();
     res.json(exhibits);
   }
 }
