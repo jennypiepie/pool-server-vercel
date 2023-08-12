bbc947a3171db4a08f24ff68624564ce42ba6cac
派派 <2416655925@qq.com>
2023-08-13 00:35:20 +0800

    NOT userName

--- api/updateLikes.ts
+++ api/updateLikes.ts
@@ -7,12 +7,12 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
   if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
   const db: Db | null = await connectToDb() || null;
     if (db) {
-        const { exhibitsName, userName } = req.body;
+        const { exhibitsName, username } = req.body;
         const collection = db.collection('exhibits');
         const exhibits = await collection.find({ name: exhibitsName }).toArray();
         const likes = exhibits[0].likes;
-        const index = likes.findIndex(i => i === userName);
-        index === -1 ? likes.push(userName) : likes.splice(index, 1);
+        const index = likes.findIndex(i => i === username);
+        index === -1 ? likes.push(username) : likes.splice(index, 1);
         collection.updateOne(
             { name: exhibitsName },
             { $set: { likes } }
