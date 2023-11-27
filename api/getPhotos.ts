590728d8304c69fb2d069dc53deb5c57e197a3a8
派派 <2416655925@qq.com>
2023-11-27 16:52:02 +0800

    get photos

--- api/getPhotos.ts
+++ api/getPhotos.ts
@@ -2,13 +2,15 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { Db } from 'mongodb'
 import { connectToDb } from '../lib/db';
 
+
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
     if (req.method === 'OPTIONS') { return res.json(({ body: "OK" })) }
+    const { username } = req.body || {};
+
     const db: Db | null = await connectToDb() || null;
     if (db) {
-        const { username, base64 } = req.body;
         const collection = db.collection('photos');
-        collection.insertOne({ base64, user: username });
-        res.json({ message: '添加成功' });
+        const photos = await collection.find({ user: username }).toArray();
+        res.json(photos);
     }
 }
